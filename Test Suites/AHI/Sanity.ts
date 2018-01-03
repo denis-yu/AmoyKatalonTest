35651a0d8ff9609fb3d9a6d1641f11907d9ee2e3
denis-yu <denis.yu@suryani.cn>
2018-01-03 17:31:05 +0800

    new

---
+++ Test Suites/AHI/Sanity.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Sanity</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>2d53abbb-31bd-4e35-93c7-1efe91d1205d</testSuiteGuid>
+</TestSuiteEntity>
